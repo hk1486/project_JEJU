08085521b7d3d24bb0e2e03403a8473c99df3eeb
ubuntu <hk1486@naver.com>
2024-09-12 15:45:48 +0900

    modify main

--- main.py
+++ main.py
@@ -15,4 +15,4 @@ app.include_router(receive_onboarding_info, prefix="/jeju")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=48000)
