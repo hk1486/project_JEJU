4d9468b1e3c30d3ffcd577fe4507325ebc505b2c
ubuntu <hk1486@naver.com>
2024-10-19 09:39:30 +0900

    modify main.py

--- main.py
+++ main.py
@@ -2,21 +2,27 @@ from fastapi import FastAPI
 import warnings
 warnings.filterwarnings('ignore')
 
+# 사용자 관련 라우터 임포트
 from login.insert_user_info import router as user_info_router
-# from travel.call_travel_items import router as culture_info_router
-from onboarding.onboarding_user_info import router as receive_onboarding_info
 from login.select_user_id import router as login
+from onboarding.onboarding_user_info import router as receive_onboarding_info
+from user.select_information import router as my_info_router
+from user.update_information import router as update_info_router
+from user.select_like_list import router as like_list_router
+
+# 관광아이템 관련 라우터 임포트
 from travel.call_main_page_items import router as main_layout
 from travel.call_travel_item_details import router as details_layout
 from travel.likes import router as like_router
 from travel.main_category_items import router as category
+
+# AIdocent 관련 라우터 임포트
 from docentAI.nearby_spot_title import router as docent_nearby
 from docentAI.recommend_list import router as recommend_list
 from docentAI.travel_course import router as recommend_travel
+
+# 내 코스 관련 라우터 임포트
 from course.router import router as course_router
-from user.select_information import router as my_info_router
-from user.update_information import router as update_info_router
-from user.select_like_list import router as like_list_router
 from course.create import router as create_course_router
 from course.select import router as select_course_router
 from course.insert import router as insert_course_router
